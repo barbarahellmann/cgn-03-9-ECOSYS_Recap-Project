8ce360d084b49650949e98d5144c3cf1a62b97d8
barbarahellmann <babshellmann@web.de>
2024-03-30 14:45:28 +0100

    Dank Branchen wieder 10 Schritte zurück.

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -3,24 +3,24 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
 
- public Order addOrder(List<String> productIds) {
+ public Optional<Order> addOrder(List<String> productIds) throws NoProductException {
 
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             try {
-            Product productToOrder = productRepo.getProductById(productId);
-                products.add(productToOrder);}
-               catch (Exception NoProductException){
+                Optional<Product> productToOrder = productRepo.getProductById(productId);
+                productToOrder.ifPresent(products::add);
+            } catch (Exception NoProductException) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
         }
-
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return Optional.ofNullable(orderRepo.addOrder(newOrder));
